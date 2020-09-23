77120b4677851675a11787ae33309bda7f014240
massimoHeinrich <massimo-heinrich@t-online.de>
2020-09-23 09:55:45 +0200

    Aufagbe 6 gelöst (nachtrag)

--- Aufgaben/Arrays.c
+++ Aufgaben/Arrays.c
@@ -179,6 +179,7 @@ void arraysMenue() {
                 buchstabenanzahl();
                 break;
             case 6:
+                buchstabenanzahlSatz();
                 break;
             case 7:
                 break;
