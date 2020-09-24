a2e7b04ff80802521bd29b16732df833e279c4d3
massimoHeinrich <massimo-heinrich@t-online.de>
2020-09-24 12:47:44 +0200

    Aufgabe 8 vorbereitet, Aufgabe 6 Anpassungen

--- Aufgaben/Sortierverfahren.c
+++ Aufgaben/Sortierverfahren.c
@@ -4,6 +4,7 @@
 #include "../AllgemeineFunktionen.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Aufgabe 2
 int tabelleForXSchleife() {
@@ -35,7 +36,7 @@ int tabelleForXSchleife() {
 }
 
 // Aufgabe 6
-int tabelleForXSchleife10Werte() {
+int sortieren10Werte() {
     int zahlen[11];
     int x,y,merker;
     int i = 0;
@@ -59,13 +60,20 @@ int tabelleForXSchleife10Werte() {
                 zahlen[x] = zahlen[y];
                 zahlen[y] = merker;
             }
-            printf("X-Durchlauf: %d, y: %d, zahlen[y]: %d \n", i, y, zahlen[y]);
+            // für übersichtlichere Ausgabe auskommentiert (nicht in Aufgabe)
+            // printf("X-Durchlauf: %d, y: %d, zahlen[y]: %d \n", i, y, zahlen[y]);
         }
-        printf("X-Durchlauf: %d, x: %d, zahlen[x]: %d\n", i, x, zahlen[x]);
+        // für übersichtlichere Ausgabe auskommentiert (nicht in Aufgabe)
+        // printf("X-Durchlauf: %d, x: %d, zahlen[x]: %d\n", i, x, zahlen[x]);
     }
     printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d \n",
             zahlen[1], zahlen[2], zahlen[3], zahlen[4], zahlen[5], zahlen[6],zahlen[7], zahlen[8], zahlen[9], zahlen[10]);
     return (0);
+}
+
+// Aufgabe 8
+void wortlisteSortieren() {
+    // ToDo
 }
 
 void sortierVerfahrenMenue() {
@@ -73,7 +81,8 @@ void sortierVerfahrenMenue() {
         clearScreen();
         puts("Aufgaben zu Sortierverfahren:");
         puts("1: Zahlenwerte der for x Schleife");
-        puts("2: Zahlenwerte der for x Schleife für 10 Werte");
+        puts("2: 10 Werte sortiert");
+        puts("2: Wörter sortieren");
 
         puts("0: Zurück zum Hauptmenü");
         printf("\nTreffe eine Auswahl: ");
@@ -85,9 +94,10 @@ void sortierVerfahrenMenue() {
                 tabelleForXSchleife();
                 break;
             case 2:
-                tabelleForXSchleife10Werte();
+                sortieren10Werte();
                 break;
             case 3:
+                wortlisteSortieren();
                 break;
             case 4:
                 break;
