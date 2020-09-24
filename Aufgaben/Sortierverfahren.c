4e9da772cd6ea6347faa086a3e2b74fedafb0418
massimoHeinrich <massimo-heinrich@t-online.de>
2020-09-24 12:22:19 +0200

    Aufgabe 6 gelöst, Aufgabe 2 ergänzt, Test änderungen zurückgenommen

--- Aufgaben/Sortierverfahren.c
+++ Aufgaben/Sortierverfahren.c
@@ -29,6 +29,42 @@ int tabelleForXSchleife() {
         }
         printf("X-Durchlauf: %d, x: %d, zahlen[x]: %d\n", i, x, zahlen[x]);
     }
+    printf("%d, %d, %d, %d, %d \n",
+           zahlen[1], zahlen[2], zahlen[3], zahlen[4], zahlen[5]);
+    return (0);
+}
+
+// Aufgabe 6
+int tabelleForXSchleife10Werte() {
+    int zahlen[11];
+    int x,y,merker;
+    int i = 0;
+
+    zahlen[1] = 9;
+    zahlen[2] = 10;
+    zahlen[3] = 13;
+    zahlen[4] = 7;
+    zahlen[5] = 1;
+    zahlen[6] = 18;
+    zahlen[7] = 23;
+    zahlen[8] = 16;
+    zahlen[9] = 5;
+    zahlen[10] = 21;
+
+    for (x = 1;  x <= 9; x++) {
+        i++;
+        for (y = x + 1; y <= 10; y++) {
+            if (zahlen[x] > zahlen[y]) {
+                merker = zahlen[x];
+                zahlen[x] = zahlen[y];
+                zahlen[y] = merker;
+            }
+            printf("X-Durchlauf: %d, y: %d, zahlen[y]: %d \n", i, y, zahlen[y]);
+        }
+        printf("X-Durchlauf: %d, x: %d, zahlen[x]: %d\n", i, x, zahlen[x]);
+    }
+    printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d \n",
+            zahlen[1], zahlen[2], zahlen[3], zahlen[4], zahlen[5], zahlen[6],zahlen[7], zahlen[8], zahlen[9], zahlen[10]);
     return (0);
 }
 
@@ -37,6 +73,7 @@ void sortierVerfahrenMenue() {
         clearScreen();
         puts("Aufgaben zu Sortierverfahren:");
         puts("1: Zahlenwerte der for x Schleife");
+        puts("2: Zahlenwerte der for x Schleife für 10 Werte");
 
         puts("0: Zurück zum Hauptmenü");
         printf("\nTreffe eine Auswahl: ");
@@ -48,6 +85,7 @@ void sortierVerfahrenMenue() {
                 tabelleForXSchleife();
                 break;
             case 2:
+                tabelleForXSchleife10Werte();
                 break;
             case 3:
                 break;
--- Readme.md
+++ Readme.md
@@ -47,7 +47,6 @@ Dieses Projekt ist für den Standard C99 und den Clang Compiler ausgelegt. Verwe
     - [ ] Aufgabe 3 
     - [ ] Aufgabe 4 
     - [ ] Aufgabe 5 
-    - [ ] Aufgabe 6 
+    - [x] Aufgabe 6 
     - [ ] Aufgabe 7 
     - [ ] Aufgabe 8 
-    
