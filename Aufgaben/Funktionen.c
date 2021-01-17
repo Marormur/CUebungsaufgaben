1aad4fe14fa9bb33448344de689b0d33a312a844
massimoHeinrich <massimo-heinrich@t-online.de>
2021-01-17 17:27:11 +0100

    Funktionen Aufgabe 1 a)
    - Bug in '(3) Teiltext in Text suchen'

--- Aufgaben/Funktionen.c
+++ Aufgaben/Funktionen.c
@@ -4,11 +4,101 @@
 
 #include "Funktionen.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../AllgemeineFunktionen.h"
+
+// Aufgabe 1 a)
+
+const int a_maxLaengeZeichenfolge = 128;
+const int a_maxLaengeText = 1024;
+int a_zahl1, a_zahl2, a_ergebnis, a_zahl1M, a_zahl2M, a_ergebnisM;
+char a_wunschname[32], a_dateiname[37], a_text[a_maxLaengeText], a_zeichenfolge[a_maxLaengeZeichenfolge];
+char a_dateiEndung[] = ".txt";
+
+void a_addition() {
+    printf("Bitte geben Sie die erste Zahl ein: ");
+    scanf("%d", &a_zahl1);
+    printf("Bitte geben Sie die zweite Zahl ein: ");
+    scanf("%d", &a_zahl2);
+    a_ergebnis = a_zahl1 + a_zahl2;
+    printf("Das ergebnis ist: %d \n\n", a_ergebnis);
+}
+
+void a_dateiAnlegen() {
+    printf("Bitte geben Sie einen Dateinamen ein: ");
+    scanf("%s", a_wunschname);
+    strcpy(a_dateiname, a_wunschname);
+    strcat(a_dateiname, a_dateiEndung);
+
+    // hier erstellte Dateien werden im Ordner "cmake-build-debug" gespeichert
+    FILE * datei;
+    datei = fopen(a_dateiname, "w");
+    fclose(datei);
+
+    printf("Die Datei mit dem Namen '%s' wurde angelegt. \nDer Name ist %lu Zeichen lang\n", a_dateiname, strlen(a_dateiname));
+}
+
+// FIXME: Es wird nicht auf die erste Eingabe gewartet
+void a_teiltextInTextSuchen() {
+    printf("Bitte geben Sie den zu durchsuchenden Text ein: ");
+    fgets(a_text, a_maxLaengeText, stdin);
+
+    printf("Bitte geben Sie die zu suchende Zeichenfolge ein: ");
+    fgets(a_zeichenfolge, a_maxLaengeZeichenfolge, stdin);
+
+    if (strstr(a_text, a_zeichenfolge)) {
+        printf("Der eingegebene Text enthält die Zeichenfolge '%s'.\n", a_zeichenfolge);
+    } else {
+        printf("Der eingegebene Text enthält die Zeichenfolge '%s' nicht.\n", a_zeichenfolge);
+    }
+}
+
+void a_multiplikation() {
+    printf("Bitte geben Sie die erste Zahl ein: ");
+    scanf("%d", &a_zahl1M);
+    printf("Bitte geben Sie die zweite Zahl ein: ");
+    scanf("%d", &a_zahl2M);
+    a_ergebnisM = a_zahl1M * a_zahl2M;
+    printf("Das ergebnis ist: %d \n\n", a_ergebnisM);
+}
+
+void a_menue() {
+    while (1) {
+        clearScreen();
+        puts("Aufgabe 1a)");
+        puts("1: Addieren");
+        puts("2: Datei anlegen");
+        puts("3: Teiltext in Text suchen");
+        puts("4: Multiplizieren");
+
+        puts("0: Ende (Zurück zum Hauptmenü)");
+        printf("\nTreffe eine Auswahl: ");
+        int input = (int)strtol(getString(2), NULL, 10);
+        switch (input) {
+            case 0:
+                return;
+            case 1:
+                a_addition();
+            case 2:
+                a_dateiAnlegen();
+            case 3:
+                a_teiltextInTextSuchen();
+            case 4:
+                a_multiplikation();
+            default:
+                // Nur zur Unterdrückung einer Warnung
+                break;
+        }
+    }
+}
+
 void funktionenMenue() {
     while (1) {
         clearScreen();
         puts("Aufgaben zu Funktionen:");
-        puts("1: ");
+        puts("1: Funktionen erhalten / liefern keine Werte, alle Variablen global");
         puts("2: ");
         puts("3: ");
         puts("4: ");
@@ -20,7 +110,7 @@ void funktionenMenue() {
             case 0:
                 return;
             case 1:
-                break;
+                a_menue();
             case 2:
                 break;
             case 3:
@@ -32,3 +122,4 @@ void funktionenMenue() {
                 break;
         }
     }
+}
