82895d0c742b0377b89e5941c94c8fb1253c6897
massimoHeinrich <massimo-heinrich@t-online.de>
2021-01-25 13:27:11 +0100

    Pointer Aufgabe 1: ToDo Richtigkeit der Antwort verifizieren

--- src/Aufgaben/Pointer.c
+++ src/Aufgaben/Pointer.c
@@ -3,18 +3,27 @@
 #include "../AllgemeineFunktionen.h"
 #include "Schleifen.h"
 
+int anzahlPointer() {
+    printf("Es werden 4 Pointer erzeugt. Sie kÃ¶nnen auf die Wagen / Lokomotive zeigen."); // ToDo: PrÃ¼fen ob die Angaben richtig sind
+    return (0);
+}
+
 void pointerMenue() {
     while (1) {
         clearScreen();
         puts("Aufgaben zu Pointer:");
-        puts("0: Zurück zum Hauptmenü");
+        puts("0: Zurï¿½ck zum Hauptmenï¿½");
+        puts("1: Wie viele Pointer und worauf zeigen diese?");
         printf("\nTreffe eine Auswahl: ");
         int input = (int)strtol(getString(2), NULL, 10);
         switch (input) {
         case 0:
             return;
+        case 1:
+            anzahlPointer();
+            break;
         default:
-            // Nur zur Unterdrückung einer Warnung
+            // Nur zur Unterdrï¿½ckung einer Warnung
             break;
         }
     }
