168331208729ad9f2a9a1c1610ea94b256c97372
Marvin Temmen <github@marvintemmen.de>
2025-10-25 15:52:15 +0200

    style: Format code for consistency and readability in Pointer.c

--- src/Aufgaben/Pointer.c
+++ src/Aufgaben/Pointer.c
@@ -3,35 +3,41 @@
 #include "../AllgemeineFunktionen.h"
 #include "Schleifen.h"
 
-struct rechnungstype {
+struct rechnungstype
+{
     int zahl1, zahl2, zahl3, zahl4;
     char rechenzeichen1, rechenzeichen2;
 };
 
-int Add_1(int a, int b) {
+int Add_1(int a, int b)
+{
     return a + b;
 }
 
-void Add_Mul_1(int a, int b, int *aErg, int *mErg) {
+void Add_Mul_1(int a, int b, int *aErg, int *mErg)
+{
     *aErg = a + b;
     *mErg = a * b;
 }
 
-void Add_Mul_2(int zahlen[2], int *aErg, int *mErg) {
-    *aErg = zahlen[0] + zahlen [1];
-    *mErg = zahlen[0] * zahlen [1];
+void Add_Mul_2(int zahlen[2], int *aErg, int *mErg)
+{
+    *aErg = zahlen[0] + zahlen[1];
+    *mErg = zahlen[0] * zahlen[1];
 }
 
-int * Add_Mul_3(int zahlen[2]) {
+int *Add_Mul_3(int zahlen[2])
+{
     int ergebnis[2];
 
-    ergebnis[0] = zahlen[0] + zahlen [1];
-    ergebnis[1] = zahlen[0] * zahlen [1];
+    ergebnis[0] = zahlen[0] + zahlen[1];
+    ergebnis[1] = zahlen[0] * zahlen[1];
 
     return ergebnis;
 }
 
-struct rechnungstype Add_Mul_4(struct rechnungstype RT) {
+struct rechnungstype Add_Mul_4(struct rechnungstype RT)
+{
     // Unklar, wie rechenzeichen aus Strings verwendet werden können
     RT.zahl3 = RT.zahl1 + RT.zahl2;
     RT.zahl4 = RT.zahl1 * RT.zahl2;
@@ -39,8 +45,8 @@ struct rechnungstype Add_Mul_4(struct rechnungstype RT) {
     return RT;
 }
 
-
-int mehrereUebergabeparameter() {
+int mehrereUebergabeparameter()
+{
 
     int Zahl1, Zahl2, Zahl3, Zahl4;
     int Zahlen[2];
@@ -53,12 +59,14 @@ int mehrereUebergabeparameter() {
 
     // Add_1
     printf("Die Funktion ´Add_1´ bekommt 2 Zahlen. Diese werden addiert. Die Rückgabe erfolgt in Einzelwerten.\n"
-           "Das ergebnis der Addition ist: %d\n\n", Add_1(Zahl1, Zahl2));
+           "Das ergebnis der Addition ist: %d\n\n",
+           Add_1(Zahl1, Zahl2));
 
     // Add_Mul_1
     Add_Mul_1(Zahl1, Zahl2, &Zahl3, &Zahl4);
     printf("Die Funktion ´Add_Mul_1´ bekommt 2 Zahlen. Diese werden addiert und multipliziert. Die Rückgabe erfolgt in Einzelwerten.\n"
-           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n", Zahl3, Zahl4);
+           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n",
+           Zahl3, Zahl4);
 
     // Add_Mul_2
     Zahlen[0] = Zahl1;
@@ -66,12 +74,14 @@ int mehrereUebergabeparameter() {
 
     Add_Mul_2(Zahlen, &Zahl3, &Zahl4);
     printf("Die Funktion ´Add_Mul_2´ bekommt ein Zahlen-Array. Die enthaltenen Zahlen werden addiert und multipliziert. Die Rückgabe erfolgt in Einzelwerten.\n"
-           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n", Zahl3, Zahl4);
+           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n",
+           Zahl3, Zahl4);
 
     // Add_Mul_3
     int *ergebnisMul3 = Add_Mul_3(Zahlen);
     printf("Die Funktion ´Add_Mul_3´ bekommt ein Zahlen-Array. Die enthaltenen Zahlen werden addiert und multipliziert. Die Rückgabe erfolgt als Array. \n"
-           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n", ergebnisMul3[0], ergebnisMul3[1]);
+           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n",
+           ergebnisMul3[0], ergebnisMul3[1]);
 
     // Add_Mul_4
     rechnung.zahl1 = Zahl1;
@@ -81,21 +91,24 @@ int mehrereUebergabeparameter() {
 
     struct rechnungstype ergebnisRechnung = Add_Mul_4(rechnung);
     printf("Die Funktion ´Add_Mul_4´ bekommt ein Struct. Die enthaltenen Zahlen werden addiert und multipliziert. Die Rückgabe erfolgt als Struct. \n"
-           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n", ergebnisRechnung.zahl3, ergebnisRechnung.zahl4);
-
+           "Das ergebnis der Addition ist: %d \nDas Ergebnis der Multiplikation ist: %d\n\n",
+           ergebnisRechnung.zahl3, ergebnisRechnung.zahl4);
 
     return (0);
 }
 
-void pointerMenue() {
-    while (1) {
+void pointerMenue()
+{
+    while (1)
+    {
         clearScreen();
         puts("Aufgaben zu Pointer:");
         puts("0: Zur�ck zum Hauptmen�");
         puts("1: Mehrere Uebergabeparameter");
         printf("\nTreffe eine Auswahl: ");
         int input = (int)strtol(getString(2), NULL, 10);
-        switch (input) {
+        switch (input)
+        {
         case 0:
             return;
         case 1:
